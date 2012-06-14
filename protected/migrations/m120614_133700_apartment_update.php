ba4995c5cd2c94908a54f7f5d98a830521857c7c
Denis Boldinov <positivejob@yandex.ru>
2012-06-14 18:55:03 +0400

    missing watermark & migration

---
+++ protected/migrations/m120614_133700_apartment_update.php
@@ -0,0 +1,25 @@
+<?php
+
+class m120614_133700_apartment_update extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m120614_133700_apartment_update does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
